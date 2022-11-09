9f26535e4f9710185893b6aa1fb0fd64e0fffb79
Apoffis <abc22_08@mail.ru>
2022-11-09 15:34:05 +0400

    build maven tool commit 1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,23 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        boolean isNotEmpty = args == null || args.isEmpty();
+        if(isNotEmpty) return false;
+        return args.stream().allMatch(Utils::isNumericAndPositive);
+    }
+
+    private static boolean isNumericAndPositive(String strNum) {
+        if (strNum == null || strNum.isEmpty()) {
+            return false;
+        }
+        try {
+            if (Double.parseDouble(strNum) > 0) {
+                return true;
+            }
+
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return false;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (str == null || str.isEmpty()) return false;
+        return Double.parseDouble(str) > 0;
     }
 }
